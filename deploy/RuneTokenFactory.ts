5f894e386a5bb814bf1ba8c82d9fb1161b8c1c8f
Luis Videla <luisvid@gmail.com>
2024-09-07 17:26:28 -0300

    test: interaction with deployed contracts

---
+++ deploy/RuneTokenFactory.ts
@@ -0,0 +1,38 @@
+import assert from 'assert'
+
+import { type DeployFunction } from 'hardhat-deploy/types'
+
+const contractName = 'RuneTokenFactory'
+
+// commands to run:
+// npx hardhat lz:deploy --tags RuneTokenFactory
+// npx hardhat lz:deploy --tags RuneTokenFactory --networks base-sepolia
+
+const deploy: DeployFunction = async (hre) => {
+    const { getNamedAccounts, deployments } = hre
+
+    const { deploy } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    assert(deployer, 'Missing named deployer account')
+
+    console.log(`Network: ${hre.network.name}`)
+    console.log(`Deployer: ${deployer}`)
+
+    const endpointV2Deployment = await hre.deployments.get('EndpointV2')
+
+    const { address } = await deploy(contractName, {
+        from: deployer,
+        args: [
+            endpointV2Deployment.address, // LayerZero's EndpointV2 address
+        ],
+        log: true,
+        skipIfAlreadyDeployed: false,
+    })
+
+    console.log(`Deployed contract: ${contractName}, network: ${hre.network.name}, address: ${address}`)
+}
+
+deploy.tags = [contractName]
+
+export default deploy
